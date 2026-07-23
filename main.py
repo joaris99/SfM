6ed0bc0f1285393258c07c9e7729d5b5b0d7f597
unknown <joagr470@student.liu.se>
2026-07-24 00:46:35 +0200

    changed sfm to consider multiple images per view

--- main.py
+++ main.py
@@ -22,21 +22,55 @@ sys.path.append("build/Release")
 
 data_path = "images/my"
 recon = data_structure.Reconstruction()
+threshold = 5
+angle_threshold = 0.5
 
 images, K, distortion = sfm.load_data(data_path)
+print(K, distortion)
 num_images = len(images)
-sfm.setup(recon, images[0], images[1], K)
+sfm.setup(recon, images[0], images[1], K, angle_threshold=angle_threshold)
+
+debug.plot_3D(recon)
 
 with log_indent():
     for iteration in tqdm(range(num_images - 2), desc="Incremental SfM"):
         logger.info(f"iteration {iteration}:")
         sfm.ba(recon, K, iteration)
-
-        prev_view = recon.views[iteration + 1]
-        im_prev = images[iteration + 1]
-        im_next = images[iteration + 2]
         
-        sfm.two_view_recon(recon, prev_view, im_prev, im_next, K, iteration)
+        # debug.plot_3D(recon)
+
+        im_next = images[iteration + 2]
+        candidate_views = sfm.select_candidate_views(iteration)
+        object_points = np.empty((0,3))
+        image_points = np.empty((0,2))
+        correspondence_map = []
+        matches = []
+
+        for prev_idx in candidate_views:
+            prev_view = recon.views[prev_idx]
+            result  = sfm.find_correspondences(recon, prev_view, im_next)
+            object_points = np.vstack((object_points, result["object_points"]))
+            image_points = np.vstack((image_points, result["image_points"]))
+
+            for local_idx in range(len(result["object_points"])):
+                correspondence_map.append((len(matches), local_idx))
+
+            matches.append(result)
+
+        inliers, R, t = sfm.estimate_pose(K, object_points, image_points)
+        view_id = recon.add_view(R, t, matches[0]["kp_next"], matches[0]["desc_next"], im_next)
+
+        with log_time("add old observations"):
+            for global_idx in inliers.ravel():
+                match_id, local_idx = correspondence_map[global_idx]
+                result = matches[match_id]
+                prev_idx = candidate_views[match_id]
+                prev_view = recon.views[prev_idx]
+                sfm.add_old_obs(recon, prev_view, view_id, np.array([local_idx]), result["match_indices"], result["idx1"], result["idx2"], result["pts2"])
+
+        for prev_idx, result in zip(candidate_views, matches):
+            prev_view = recon.views[prev_idx]
+            sfm.triangulate_new_points(recon, K, prev_view, view_id, result["unmatched_indices"], result["pts1"], result["pts2"], result["idx1"], result["idx2"], iteration, threshold=threshold,  angle_threshold=angle_threshold)
 
 sfm.finalize_recon(recon, num_images)
 sfm.compute_error(recon, K, verbose = True)
--- sfm.py
+++ sfm.py
@@ -5,6 +5,7 @@ import cv2
 import numpy as np
 import geometry
 import bundle_adjustment
+import debug
 from pathlib import Path
 
 def load_data(data_path):
@@ -42,7 +43,7 @@ def load_data(data_path):
     return images, K, distortion_coefficients
 
 
-def setup(recon, im1, im2, K): 
+def setup(recon, im1, im2, K, angle_threshold = 0.5): 
     """
     initiates SfM with the first 2 views
     """ 
@@ -57,7 +58,7 @@ def setup(recon, im1, im2, K):
         E, mask_E = cv2.findEssentialMat(pts1, pts2, K,  method=cv2.RANSAC, prob=0.9999, threshold=0.5)
         mask_E = mask_E.ravel().astype(bool)
         matches, pts1, pts2, idx1, idx2 = matches[mask_E], pts1[mask_E], pts2[mask_E], idx1[mask_E], idx2[mask_E]
-        
+    # debug.plot_correspondences(im1, im2, pts1, pts2)
 
     with log_time("recover pose"):
         # Recover relative pose
@@ -82,7 +83,7 @@ def setup(recon, im1, im2, K):
             # Reject points with to low angle
             angle = geometry.triangulation_angle(point, np.identity(3), np.array([[0], [0], [0]]), R, t)
             
-            if angle < 0.5:
+            if angle < angle_threshold:
                 continue
 
             point_id = recon.add_point(point, 0)
@@ -90,13 +91,39 @@ def setup(recon, im1, im2, K):
             recon.add_observation(xy=pts1[i], view_id=v1_id, point_id=point_id, feature_idx=idx1[i])
             recon.add_observation(xy=pts2[i], view_id=v2_id, point_id=point_id, feature_idx=idx2[i])
 
-def two_view_recon(recon, prev_view, im_prev, im_next, K, iteration):
+
+def select_candidate_views(iteration):
+    current_idx = iteration + 2
+
+    candidate_views = []
+    # Last 5 views
+    for offset in range(1, 6):
+        idx = current_idx - offset
+        if idx >= 0:
+            candidate_views.append(idx)
+
+    # 3 evenly spaced older views
+    if current_idx > 5:
+        older = np.linspace(
+            0,
+            current_idx - 6,
+            min(3, current_idx - 5),
+            dtype=int
+        )
+        candidate_views.extend(older.tolist())
+
+    # Remove duplicates while preserving order
+    candidate_views = list(dict.fromkeys(candidate_views))
+
+    return candidate_views
+
+def find_correspondences(recon, prev_view, im_next):
+    im_prev = prev_view.image
 
     with log_time("find correspondences"):
         kp_prev, kp_next, desc_prev, desc_next, matches = correspondences.find_correspondences_akaze(im_prev, im_next)
         pts1, pts2, idx1, idx2 = correspondences.get_coordinates(kp_prev, kp_next, matches)
-        pts1_norm = cv2.undistortPoints(pts1.reshape(-1, 1, 2), K, None).reshape(-1, 2)
-        pts2_norm = cv2.undistortPoints(pts2.reshape(-1, 1, 2), K, None).reshape(-1, 2)
+        
         
     with log_time("find observation point matches"):
         object_points = []
@@ -116,7 +143,25 @@ def two_view_recon(recon, prev_view, im_prev, im_next, K, iteration):
     
         object_points = np.asarray(object_points, dtype=np.float32)
         image_points = np.asarray(image_points, dtype=np.float32)
+    
+    return {
+        "kp_next": kp_next,
+        "desc_next": desc_next,
 
+        "pts1": pts1,
+        "pts2": pts2,
+
+        "idx1": idx1,
+        "idx2": idx2,
+
+        "match_indices": match_indices,
+        "unmatched_indices": unmatched_indices,
+
+        "object_points": np.asarray(object_points, dtype=np.float32),
+        "image_points": np.asarray(image_points, dtype=np.float32),
+    }
+
+def estimate_pose(K, object_points, image_points):
     with log_time("pnp"):
         success, rvec, tvec, inliers = cv2.solvePnPRansac(object_points, image_points, K, None, reprojectionError=4.0, 
                                                         confidence=0.999, iterationsCount=1000, flags=cv2.SOLVEPNP_ITERATIVE)
@@ -124,28 +169,54 @@ def two_view_recon(recon, prev_view, im_prev, im_next, K, iteration):
             raise RuntimeError("PnP failed")
         R, _ = cv2.Rodrigues(rvec)
         t = tvec.reshape(3)
-    
-    with log_time("add views and observations"):
-        view_id = recon.add_view(R, t, kp_next, desc_next, im_next)
-        for j in inliers.ravel():
-            i = match_indices[j]      # original match index
-            obs_id = prev_view.feature_to_observation[idx1[i]]
-            point_id = recon.observations[obs_id].point_id
 
-            recon.add_observation(xy=pts2[i], view_id=view_id, point_id=point_id, feature_idx=idx2[i])
-    
+    return inliers, R, t
+
+
+def add_old_obs(recon, prev_view, view_id, inliers, match_indices, idx1, idx2, pts2):
+    new_view = recon.views[view_id]
+
+    for j in inliers.ravel():
+        i = match_indices[j]
+
+        obs_id = prev_view.feature_to_observation[idx1[i]]
+        point_id = recon.observations[obs_id].point_id
+
+        feature_idx = idx2[i]
+
+        # Already added this feature from another view
+        if feature_idx in new_view.feature_to_observation:
+            continue
+
+        recon.add_observation(
+            xy=pts2[i],
+            view_id=view_id,
+            point_id=point_id,
+            feature_idx=feature_idx
+        )
+
+
+def triangulate_new_points(recon, K, prev_view, view_id, unmatched_indices, pts1, pts2, idx1, idx2, iteration, threshold = 2, angle_threshold = 0.5):
+
     C_prev = np.concat((prev_view.R, prev_view.t.reshape(3,1)), axis = 1)
     C_next = np.concat((recon.views[view_id].R, recon.views[view_id].t.reshape(3,1)), axis = 1)
-
-    threshold = 2
+    with log_time("normalize points"):
+        pts1_norm = cv2.undistortPoints(pts1.reshape(-1, 1, 2), K, None).reshape(-1, 2)
+        pts2_norm = cv2.undistortPoints(pts2.reshape(-1, 1, 2), K, None).reshape(-1, 2)
     
     for i in unmatched_indices:
+
+        # make sure point is not already added
+        next_view = recon.views[view_id]
+        if idx2[i] in next_view.feature_to_observation:
+            continue
+
         point = geometry.triangulate_optimal(C_prev, C_next, pts1_norm[i], pts2_norm[i])
 
         # Reject points with to low angle
         angle = geometry.triangulation_angle(point, prev_view.R, prev_view.t.reshape(3,1), recon.views[view_id].R, recon.views[view_id].t.reshape(3,1))
 
-        if angle < 0.5:
+        if angle < angle_threshold:
             continue
 
         # Reject points behind either camera
@@ -168,6 +239,7 @@ def two_view_recon(recon, prev_view, im_prev, im_next, K, iteration):
         feat_next = idx2[i]
         recon.add_observation(xy=pts1[i], view_id=prev_view.id, point_id=point_id, feature_idx=feat_prev,)
         recon.add_observation(xy=pts2[i], view_id=view_id, point_id=point_id, feature_idx=feat_next)
+
 
 def ba(recon, K, iteration):
     with log_time("packet info for BA"):
@@ -211,6 +283,7 @@ def finalize_recon(recon, num_images):
                     points_to_remove.append(point.id)
         for i in points_to_remove:
             recon.remove_point(i)
+
 
 def compute_error(recon, K, verbose = False):
     with log_time("compute reprojection error"):
